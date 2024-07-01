5ab209bad782bbfd0e3dfc87365a1fab95817a0f
danie <danieljavierguzzi@gmail.com>
2024-06-30 23:43:58 -0300

    Upgrades now change the character select screen

--- src/output/upgradeManager/UpgradeManager.cpp
+++ src/output/upgradeManager/UpgradeManager.cpp
@@ -2,8 +2,16 @@
 
 void UpgradeManager::GiveUpgrade(const Upgrades upgrade)
 {
-     if (Current_CharObj2 != nullptr)
-         Current_CharObj2->Upgrades |= upgrade;
+    if (Current_CharObj2 != nullptr)
+        Current_CharObj2->Upgrades |= upgrade;
+    if (CharObj2Ptrs != nullptr)
+    {
+        for (const auto& character : CharObj2Ptrs)
+        {
+            if (character != nullptr)
+                character->Upgrades |= upgrade;
+        }
+    }
 }
 
 
@@ -11,4 +19,10 @@ void UpgradeManager::RemoveUpgrade(const Upgrades upgrade)
 {
     if (Current_CharObj2 != nullptr)
         Current_CharObj2->Upgrades &= ~upgrade;
+
+    for (const auto& character : CharObj2Ptrs)
+    {
+        if (character != nullptr)
+            character->Upgrades &= ~upgrade;
+    }
 }
