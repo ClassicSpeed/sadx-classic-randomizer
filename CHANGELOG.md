55523878d3e6e8c54bce697f9a76259a77264a58
Classic <ClassicSpeed@users.noreply.github.com>
2024-08-25 17:01:42 -0300

    #197 Moved the city hall emblem a little higher

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,6 +25,7 @@ All notable changes to this project will be documented in this file.
 - Improved emblem requirements calculation.
     - It should hopefully fix the issue with the wrong emblem percentage goal.
 - Fixed soft lock when losing any race as Tails.
+- Moved the City Hall emblem a little higher so you can get it when using the Cream the Rabbit mod.
 
 ## [v0.7.1] - 2024-08-22
 
--- src/output/worldStateManager/WorldStateManager.cpp
+++ src/output/worldStateManager/WorldStateManager.cpp
@@ -566,6 +566,21 @@ FunctionHook<void> onCountSetItemsMaybe(0x0046BD20, []()-> void
     AddSetToLevel(WARP_FROM_PAST, LevelAndActIDs_Past2, Characters_Big);
 });
 
+
+// We move the emblem a little higher so Cream can get it.
+FunctionHook<void, task*> onEmblemMain(0x4B4940, [](task* tp)-> void
+{
+    if (levelact(CurrentLevel, CurrentAct) == LevelAndActIDs_StationSquare1)
+        if (tp->twp->pos.x > 386 && tp->twp->pos.x < 390
+            && tp->twp->pos.y > -6 && tp->twp->pos.y < -5
+            && tp->twp->pos.z > 489 && tp->twp->pos.z < 492)
+        {
+            tp->twp->pos.y = -4;
+        }
+
+    onEmblemMain.Original(tp);
+});
+
 FunctionHook<void> onMissionSetLoad(0x591A70, []()-> void
 {
     if (!worldStateManagerPtr->options.missionModeEnabled)
