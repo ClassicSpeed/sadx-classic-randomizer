949799aab07d43cd6a1c5a8220b9b4a00f7ed94b
Classic <ClassicSpeed@users.noreply.github.com>
2025-05-28 15:40:06 -0300

    #552 Updated archipelago dependencies

--- lib/APCpp/Archipelago.h
+++ lib/APCpp/Archipelago.h
@@ -4,12 +4,17 @@
 #include <vector>
 #include <map>
 #include <cstdint>
+#include <set>
+#include <functional>
 
 void AP_Init(const char*, const char*, const char*, const char*);
 void AP_Init(const char*);
 bool AP_IsInit();
 
 void AP_Start();
+
+// AP_Shutdown resets the library state to before initialization, and doesn't just disconnect!
+void AP_Shutdown();
 
 struct AP_NetworkVersion {
     int major;
@@ -47,31 +52,34 @@ void AP_SetDeathLinkSupported(bool);
 /* Required Callback Functions */
 
 //Parameter Function must reset local state
-void AP_SetItemClearCallback(void (*f_itemclr)());
+void AP_SetItemClearCallback(std::function<void()> f_itemclr);
 //Parameter Function must collect item id given with parameter. Secound parameter indicates whether or not to notify player
-void AP_SetItemRecvCallback(void (*f_itemrecv)(int64_t,bool));
+void AP_SetItemRecvCallback(std::function<void(int64_t,bool)> f_itemrecv);
 //Parameter Function must mark given location id as checked
-void AP_SetLocationCheckedCallback(void (*f_locrecv)(int64_t));
+void AP_SetLocationCheckedCallback(std::function<void(int64_t)> f_locrecv);
 
 /* Optional Callback Functions */
 
 //Parameter Function will be called when Death Link is received. Alternative to Pending/Clear usage
-void AP_SetDeathLinkRecvCallback(void (*f_deathrecv)());
+void AP_SetDeathLinkRecvCallback(std::function<void()> f_deathrecv);
+//Overload with the deathlink source and cause
+void AP_SetDeathLinkRecvCallback(std::function<void(std::string, std::string)> f_deathrecv);
 
 // Parameter Function receives Slotdata of respective type
-void AP_RegisterSlotDataIntCallback(std::string, void (*f_slotdata)(int));
-void AP_RegisterSlotDataMapIntIntCallback(std::string, void (*f_slotdata)(std::map<int,int>));
-void AP_RegisterSlotDataRawCallback(std::string, void (*f_slotdata)(std::string));
+void AP_RegisterSlotDataIntCallback(std::string, std::function<void(int)> f_slotdata);
+void AP_RegisterSlotDataMapIntIntCallback(std::string, std::function<void(std::map<int,int>)> f_slotdata);
+void AP_RegisterSlotDataRawCallback(std::string, std::function<void(std::string)> f_slotdata);
 
 // Send LocationScouts packet
-void AP_SendLocationScouts(std::vector<int64_t> const& locations, int create_as_hint);
+void AP_SendLocationScouts(std::set<int64_t> const& locations, int create_as_hint);
 // Receive Function for LocationInfo
-void AP_SetLocationInfoCallback(void (*f_locrecv)(std::vector<AP_NetworkItem>));
+void AP_SetLocationInfoCallback(std::function<void(std::vector<AP_NetworkItem>)> f_locinfrecv);
 
 /* Game Management Functions */
 
 // Sends LocationCheck for given index
-void AP_SendItem(int64_t);
+void AP_SendItem(int64_t location);
+void AP_SendItem(std::set<int64_t> const& locations);
 
 // Called when Story completed, sends StatusUpdate
 void AP_StoryComplete();
@@ -151,7 +159,7 @@ struct AP_RoomInfo {
 
 int AP_GetRoomInfo(AP_RoomInfo*);
 AP_ConnectionStatus AP_GetConnectionStatus();
-int AP_GetUUID();
+std::uint64_t AP_GetUUID();
 int AP_GetPlayerID();
 
 /* Serverside Data Types */
@@ -212,17 +220,17 @@ std::string AP_GetPrivateServerDataPrefix();
 // Parameter Function receives all SetReply's
 // ! Pointers in AP_SetReply struct only valid within function !
 // If values are required beyond that a copy is needed
-void AP_RegisterSetReplyCallback(void (*f_setreply)(AP_SetReply));
+void AP_RegisterSetReplyCallback(std::function<void(AP_SetReply)> f_setreply);
 
 // Receive all SetReplys with Keys in parameter list
 void AP_SetNotify(std::map<std::string,AP_DataType>);
 // Single Key version of above for convenience
 void AP_SetNotify(std::string, AP_DataType);
 
-void AP_SetTags(std::vector<std::string> tags);
-
 // Send Bounce package
 void AP_SendBounce(AP_Bounce);
 
 // Receive Bounced packages. Disables automatic DeathLink management
-void AP_RegisterBouncedCallback(void (*f_bounced)(AP_Bounce));
+void AP_RegisterBouncedCallback(std::function<void(AP_Bounce)> f_bounced);
+
+void AP_SetTags(std::vector<std::string> tags);
