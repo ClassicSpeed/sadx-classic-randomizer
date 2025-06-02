23e229765a4cbeee9a1280761226def043af4d60
Classic <ClassicSpeed@users.noreply.github.com>
2025-06-01 22:05:50 -0300

    #552 Refactor initialization of managers in Init function for improved readability and efficiency

--- src/mod.cpp
+++ src/mod.cpp
@@ -1,5 +1,3 @@
-#include <chrono>
-
 #include "pch.h"
 
 #include "application/Randomizer.h"
@@ -13,43 +11,44 @@
 #include "output/saveFileManager/SaveFileManager.h"
 #include "output/worldStateManager/WorldStateManager.h"
 
-
 extern "C" {
 std::vector<IOnFrame*> onFrameObjects;
 
 __declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 {
-    Settings* settings = &Settings::Init(path, helperFunctions);
-    Options* options = &Options::Init(*settings);
-    ReactionManager* reactionManager = &ReactionManager::Init(*options, *settings);
-    DisplayManager* displayManager = &DisplayManager::Init(*options, *settings, path, helperFunctions);
-    CharacterManager* characterManager = &CharacterManager::Init(*options, *settings, *reactionManager);
-    WorldStateManager* worldStateManager = &WorldStateManager::Init(*options, *settings);
-    ItemRepository* itemRepository = &ItemRepository::Init();
-    LocationRepository* checkRepository = &LocationRepository::Init();
-    ArchipelagoMessenger* archipelagoMessenger = &ArchipelagoMessenger::Init(*options);
-    SaveFileManager* saveFileManager = &SaveFileManager::Init();
-    MusicManager* musicManager = &MusicManager::Init(*options, *settings, helperFunctions);
-    Randomizer* randomizer = &Randomizer::Init(*options, *settings, *displayManager, *characterManager,
-                                               *worldStateManager,
-                                               *itemRepository,
-                                               *checkRepository, *archipelagoMessenger, *saveFileManager, *musicManager,
-                                               *reactionManager);
-    CheatsManager* cheatsManager = &CheatsManager::Init(*settings);
-    ArchipelagoManager* archipelagoManager = &ArchipelagoManager::Init(*options, *settings, *randomizer);
-    EventDetector* eventDetector = &EventDetector::Init(*options, *settings, *randomizer);
-    CharacterLoadingDetector* characterLoadingDetector = &CharacterLoadingDetector::Init(*randomizer);
+    Settings& settings = Settings::Init(path, helperFunctions);
+    Options& options = Options::Init(settings);
+    ReactionManager& reactionManager = ReactionManager::Init(options, settings);
+    DisplayManager& displayManager = DisplayManager::Init(options, settings, path, helperFunctions);
+    CharacterManager& characterManager = CharacterManager::Init(options, settings, reactionManager);
+    WorldStateManager& worldStateManager = WorldStateManager::Init(options, settings);
+    ItemRepository& itemRepository = ItemRepository::Init();
+    LocationRepository& locationRepository = LocationRepository::Init();
+    ArchipelagoMessenger& archipelagoMessenger = ArchipelagoMessenger::Init(options);
+    SaveFileManager& saveFileManager = SaveFileManager::Init();
+    MusicManager& musicManager = MusicManager::Init(options, settings, helperFunctions);
+
+    Randomizer& randomizer = Randomizer::Init(options, settings, displayManager, characterManager,
+                                              worldStateManager, itemRepository, locationRepository,
+                                              archipelagoMessenger, saveFileManager, musicManager, reactionManager);
+
+    CheatsManager& cheatsManager = CheatsManager::Init(settings);
+    ArchipelagoManager& archipelagoManager = ArchipelagoManager::Init(options, settings, randomizer);
+    EventDetector& eventDetector = EventDetector::Init(options, settings, randomizer);
+    CharacterLoadingDetector& characterLoadingDetector = CharacterLoadingDetector::Init(randomizer);
+
     onFrameObjects = {
-        saveFileManager, archipelagoManager, displayManager, worldStateManager,
-        eventDetector, characterLoadingDetector, randomizer, characterManager
+        &saveFileManager, &archipelagoManager, &displayManager, &worldStateManager,
+        &eventDetector, &characterLoadingDetector, &randomizer, &characterManager
     };
 }
 
 __declspec(dllexport) void __cdecl OnFrame()
 {
-    for (auto* obj : onFrameObjects)
-        obj->OnFrame();
+    for (const auto manager : onFrameObjects)
+        manager->OnFrame();
 }
 
-__declspec(dllexport) ModInfo SADXModInfo = {ModLoaderVer}; // This is needed for the Mod Loader to recognize the DLL.
+// Required for Mod Loader to recognize the DLL
+__declspec(dllexport) ModInfo SADXModInfo = {ModLoaderVer};
 }
