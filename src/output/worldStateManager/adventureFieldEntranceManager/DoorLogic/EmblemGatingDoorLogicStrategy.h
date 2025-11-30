cbe0d475837db3668d7d5f2135f9c9d008f82653
Classic <ClassicSpeed@users.noreply.github.com>
2025-11-29 21:05:19 -0300

    #554 Fix alternative entrances being open

--- src/output/worldStateManager/adventureFieldEntranceManager/DoorLogic/EmblemGatingDoorLogicStrategy.h
+++ src/output/worldStateManager/adventureFieldEntranceManager/DoorLogic/EmblemGatingDoorLogicStrategy.h
@@ -21,11 +21,43 @@ public:
 
         //Check if door leads to level for the player
         auto entrance = _adventureFieldEntranceMap.FindEntranceById(oppositeEntrance);
+
         //Static entrance
         if (entrance == nullptr)
             return _gameStatus.unlock.currentEmblems >= entranceValue->second;
+        // Is level for character
         if (_adventureFieldEntranceMap.CalculateCorrectAct(entrance->levelAndActId) == LevelAndActIDs_HedgehogHammer)
             return false;
+
+        //TODO: only false in full rando
+        // We prevent characters from using wrong entrances
+        if (true)
+        {
+            if (entranceId == SsMainToSpeedHighway)
+                if (CurrentCharacter == Characters_Knuckles)
+                    return false;
+            if (entranceId == CityHallToSpeedHighway)
+                if (CurrentCharacter != Characters_Knuckles)
+                    return false;
+            if (entranceId == JungleToLostWorld)
+                if (CurrentCharacter == Characters_Knuckles)
+                    return false;
+            if (entranceId == JungleToLostWorldAlternative)
+                if (CurrentCharacter != Characters_Knuckles)
+                    return false;
+            if (entranceId == FinalEggTowerToFinalEgg)
+                if (CurrentCharacter == Characters_Gamma)
+                    return false;
+            if (entranceId == FinalEggTowerToFinalEggAlternative)
+                if (CurrentCharacter != Characters_Gamma)
+                    return false;
+            if (entranceId == BridgeToSkyDeck)
+                if (CurrentCharacter == Characters_Knuckles)
+                    return false;
+            if (entranceId == PoolToSkyDeck)
+                if (CurrentCharacter != Characters_Knuckles)
+                    return false;
+        }
 
         if (entranceValue != _options.entranceEmblemValueMap.end())
             return _gameStatus.unlock.currentEmblems >= entranceValue->second;
