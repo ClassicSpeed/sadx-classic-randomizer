aae2d24c0f424eb0c79ca27d47a2908b0601a39b
Classic <danieljavierguzzi@gmail.com>
2026-04-13 15:17:43 -0300

    Linux: Fixed functionHook issues

--- src/mod2.cpp
+++ src/mod2.cpp
@@ -2,15 +2,29 @@
 #include "settings/Settings.h"
 #include "options/Options.h"
 #include "gameStatus/GameStatus.h"
+#include "output/reactionManager/ReactionManager.h"
 
 extern "C" {
 __declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 {
     
     //Configuration
+
     Settings& settings = Settings::Init(path, helperFunctions);
     Options& options = Options::Init(settings);
     GameStatus& gameStatus = GameStatus::Init(options);
+
+
+    // Output Managers
+    ReactionManager& reactionManager = ReactionManager::Init(settings, gameStatus);
+    // DisplayManager& displayManager = DisplayManager::Init(options, settings, gameStatus, path, helperFunctions);
+    // CharacterManager& characterManager = CharacterManager::Init(options, settings, gameStatus, reactionManager);
+    // ItemRepository& itemRepository = ItemRepository::Init(gameStatus);
+    // LocationRepository& locationRepository = LocationRepository::Init(options, gameStatus);
+    // ArchipelagoMessenger& archipelagoMessenger = ArchipelagoMessenger::Init(options, gameStatus);
+    // WorldStateManager& worldStateManager = WorldStateManager::Init(options, settings, gameStatus, archipelagoMessenger);
+    // SaveFileManager& saveFileManager = SaveFileManager::Init();
+    // MusicManager& musicManager = MusicManager::Init(options, settings, helperFunctions);
 }
 
 // Simple OnFrame test
