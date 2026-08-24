abeb53f24a40f90e92d84b60c04ea611488e4528
Classic <danieljavierguzzi@gmail.com>
2026-08-24 20:34:54 -0300

    #579 Fixed rotation of mr chao garden's entrance sign

--- src/output/worldStateManager/adventureFieldEntranceManager/AdventureFieldEntranceMap.cpp
+++ src/output/worldStateManager/adventureFieldEntranceManager/AdventureFieldEntranceMap.cpp
@@ -202,7 +202,7 @@ AdventureFieldEntranceMap::AdventureFieldEntranceMap(Options& options, GameStatu
         // SS Chao Garden
         {SsChaoGardenToHotel, LevelAndActIDs_SSGarden, 0, HotelToSsChaoGarden, 0, {-0.03f, 15, 234.52f}},
         // Mr Chao Garden
-        {MrChaoGardenToMrMain, LevelAndActIDs_MRGarden, 0, MrMainToMrChaoGarden, 90, {-18.19F, 30, -170}},
+        {MrChaoGardenToMrMain, LevelAndActIDs_MRGarden, 0, MrMainToMrChaoGarden, 270, {-18.19F, 30, -170}},
         // Ec ChaoGarden
         {EcChaoGardenToWarpHall, LevelAndActIDs_ECGarden, 0, WarpHallToEcChaoGarden, 0.0f, {-288.75f, 20, -99.12f}},
 
